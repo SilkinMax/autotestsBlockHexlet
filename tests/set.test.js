29649aad27ff6a949fb8a6496384020b9803247a
SilkinMax <max.silkin2017@gmail.com>
2026-04-27 12:33:12 +0300

    Теория: Утверждения

--- tests/set.test.js
+++ tests/set.test.js
@@ -1,15 +1,12 @@
-import set from '../src/set.js'
+import { strict as assert } from 'node:assert';
+import set from '../src/set.js';
 
-const obj = {}
+const obj = {};
 
-set(obj, 'key1', 'value1')
-if (obj.key1 !== 'value1') {
-  throw new Error('Функция работает неверно!')
-}
+set(obj, 'key1', 'value1');
+assert.equal(obj.key1, 'value1', 'Функция работает неверно!');
 
-set(obj, 'key1', 'next value')
-if (obj.key1 === 'next value') {
-  throw new Error('Функция работает неверно!')
-}
+set(obj, 'key1', 'next value');
+assert.notEqual(obj.key1, 'next value', 'Функция работает неверно!');
 
-console.log('Все тесты пройдены!')
+console.log('Все тесты пройдены!');
